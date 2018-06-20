4da80aac2c724eba810e92aa07eb4689beabd48d
Miguel <miguellealwebdev@gmail.com>
2018-06-20 00:12:37 -0500

    Added search component to Home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import Search from '../Modules/Search'
 
 const HomePageStyles = {
   width: '100%',
   height: '100vh',
   display: 'flex',
   justifyContent: 'center',
+  flexDirection: 'column',
   alignItems: 'center',
   padding: 0
+}
+
+const searchBarStyles = {
+  display: 'block',
+  textAlign: 'center',
 }
 
 export default class Home extends Component {
@@ -14,6 +21,10 @@ export default class Home extends Component {
     return (
       <div style={HomePageStyles}>
         <h1>E-Comm</h1>
+        <div style={searchBarStyles}>
+          <h3>Start Looking Now!</h3>
+          <Search />
+        </div>
       </div>
     );
   }
