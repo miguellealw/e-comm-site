e1a01c844ddf70feeec55b63a65554735e059d69
Miguel <miguellealwebdev@gmail.com>
2018-06-23 18:13:45 -0500

    Fixed Product folder name typo

---
+++ client/src/components/Products/NewProductForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { withFormik, Input } from 'formik';
+
+
+const NewProductForm = ({
+  values,
+  errors,
+  handleChange,
+  handleSubmit
+}) => (
+  <form onSumbit={handleSubmit}>
+    
+  </form>
+);
+
+export default withFormik({
+
+})(NewProductForm);
---
+++ client/src/components/Products/NewProductPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class NewProduct extends Component {
+  render() {
+    return (
+      <div>New Product Page</div>
+    );
+  }
+}
