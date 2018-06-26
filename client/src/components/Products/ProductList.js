e313043528979327d1de2071b9e85df100ba1e1d
Miguel <miguellealwebdev@gmail.com>
2018-06-26 01:18:34 -0500

    Display loader when product list is loading

--- client/src/components/Products/ProductList.js
+++ client/src/components/Products/ProductList.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Message, Loader } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { Query } from 'react-apollo';
 
 import Product from './Product';
 
-import MockData from '../../utils/utils';
 import GET_PRODUCTS from '../../graphql/queries/getProducts';
 
 
@@ -24,8 +23,8 @@ const ProductLink = (product) => (
 const ProductList = () => (
   <Query query={GET_PRODUCTS}>
     {({loading, error, data}) => {
-      if(loading) return "Loading products..."
-      if(error) return `Error fetching products - ${error}`
+      if(loading) return <Loader active={loading}>Loading Store...</Loader>
+      if(error) return <Message negative size="big" header={`${error}`}/>
 
       return (
         <Grid stretched columns={4} relaxed={'very'}>
